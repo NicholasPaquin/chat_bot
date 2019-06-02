cec8b4743892d3ac432ca83578f35318beb0e242
Nicholas Paquin <napaquin@edu.uwaterloo.ca>
2019-06-02 13:56:28 -0400

    Cleaned up some code

--- discord_bot.py
+++ discord_bot.py
@@ -160,31 +160,28 @@ def load_data(name, datafile_from, datafile_to, save_dir):
     return voc, pairs
 
 
-MIN_COUNT = 3    # Minimum word count threshold for trimming
+MIN_COUNT = 3
 
 
 def trim_words(voc, pairs, MIN_COUNT):
-    # Trim words used under the MIN_COUNT from the voc
     voc.trim(MIN_COUNT)
-    # Filter out pairs with trimmed words
     keep_pairs = []
     for pair in pairs:
         input_sentence = pair[0]
         output_sentence = pair[1]
         keep_input = True
         keep_output = True
-        # Check input sentence
+
         for word in input_sentence.split(' '):
             if word not in voc.word2index:
                 keep_input = False
                 break
-        # Check output sentence
+
         for word in output_sentence.split(' '):
             if word not in voc.word2index:
                 keep_output = False
                 break
 
-        # Only keep pairs that do not contain trimmed word(s) in their input or output sentence
         if keep_input and keep_output:
             keep_pairs.append(pair)
 
@@ -247,23 +244,16 @@ class EncoderRNN(nn.Module):
         self.hidden_size = hidden_size
         self.embedding = embedding
 
-        # Initialize GRU; the input_size and hidden_size params are both set to 'hidden_size'
-        #   because our input size is a word embedding with number of features == hidden_size
+
         self.gru = nn.GRU(hidden_size, hidden_size, n_layers,
                           dropout=(0 if n_layers == 1 else dropout), bidirectional=True)
 
     def forward(self, input_seq, input_lengths, hidden=None):
-        # Convert word indexes to embeddings
         embedded = self.embedding(input_seq)
-        # Pack padded batch of sequences for RNN module
         packed = nn.utils.rnn.pack_padded_sequence(embedded, input_lengths)
-        # Forward pass through GRU
         outputs, hidden = self.gru(packed, hidden)
-        # Unpack padding
         outputs, _ = nn.utils.rnn.pad_packed_sequence(outputs)
-        # Sum bidirectional GRU outputs
         outputs = outputs[:, :, :self.hidden_size] + outputs[:, : ,self.hidden_size:]
-        # Return output and final hidden state
         return outputs, hidden
 
 
@@ -293,7 +283,7 @@ class Attn(nn.Module):
         return torch.sum(self.v * energy, dim=2)
 
     def forward(self, hidden, encoder_outputs):
-        # Calculate the attention weights (energies) based on the given method
+
         if self.method == 'general':
             attn_energies = self.general_score(hidden, encoder_outputs)
         elif self.method == 'concat':
@@ -301,10 +291,8 @@ class Attn(nn.Module):
         elif self.method == 'dot':
             attn_energies = self.dot_score(hidden, encoder_outputs)
 
-        # Transpose max_length and batch_size dimensions
         attn_energies = attn_energies.t()
 
-        # Return the softmax normalized probability scores (with added dimension)
         return F.softmax(attn_energies, dim=1).unsqueeze(1)
 
 
@@ -312,14 +300,12 @@ class LuongAttnDecoderRNN(nn.Module):
     def __init__(self, attn_model, embedding, hidden_size, output_size, n_layers=1, dropout=0.1):
         super(LuongAttnDecoderRNN, self).__init__()
 
-        # Keep for reference
         self.attn_model = attn_model
         self.hidden_size = hidden_size
         self.output_size = output_size
         self.n_layers = n_layers
         self.dropout = dropout
 
-        # Define layers
         self.embedding = embedding
         self.embedding_dropout = nn.Dropout(dropout)
         self.gru = nn.GRU(hidden_size, hidden_size, n_layers, dropout=(0 if n_layers == 1 else dropout))
@@ -362,43 +348,36 @@ def maskNLLLoss(inp, target, mask):
 def train(input_variable, lengths, target_variable, mask, max_target_len, encoder, decoder, embedding,
           encoder_optimizer, decoder_optimizer, batch_size, clip, max_length=MAX_LENGTH):
 
-    # Zero gradients
     encoder_optimizer.zero_grad()
     decoder_optimizer.zero_grad()
 
-    # Set device options
     input_variable = input_variable.to(device)
     lengths = lengths.to(device)
     target_variable = target_variable.to(device)
     mask = mask.to(device)
 
-    # Initialize variables
     loss = 0
     print_losses = []
     n_totals = 0
 
-    # Forward pass through encoder
     encoder_outputs, encoder_hidden = encoder(input_variable, lengths)
 
-    # Create initial decoder input (start with SOS tokens for each sentence)
     decoder_input = torch.LongTensor([[SOS_token for _ in range(batch_size)]])
     decoder_input = decoder_input.to(device)
 
-    # Set initial decoder hidden state to the encoder's final hidden state
     decoder_hidden = encoder_hidden[:decoder.n_layers]
 
-    # Determine if we are using teacher forcing this iteration
     use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
 
-    # Forward batch of sequences through decoder one time step at a time
+
     if use_teacher_forcing:
         for t in range(max_target_len):
             decoder_output, decoder_hidden = decoder(
                 decoder_input, decoder_hidden, encoder_outputs
             )
-            # Teacher forcing: next input is current target
+
             decoder_input = target_variable[t].view(1, -1)
-            # Calculate and accumulate loss
+
             mask_loss, nTotal = maskNLLLoss(decoder_output, target_variable[t], mask[t])
             loss += mask_loss
             print_losses.append(mask_loss.item() * nTotal)
@@ -408,24 +387,21 @@ def train(input_variable, lengths, target_variable, mask, max_target_len, encode
             decoder_output, decoder_hidden = decoder(
                 decoder_input, decoder_hidden, encoder_outputs
             )
-            # No teacher forcing: next input is decoder's own current output
+
             _, topi = decoder_output.topk(1)
             decoder_input = torch.LongTensor([[topi[i][0] for i in range(batch_size)]])
             decoder_input = decoder_input.to(device)
-            # Calculate and accumulate loss
+
             mask_loss, nTotal = maskNLLLoss(decoder_output, target_variable[t], mask[t])
             loss += mask_loss
             print_losses.append(mask_loss.item() * nTotal)
             n_totals += nTotal
 
-    # Perform backpropatation
     loss.backward()
 
-    # Clip gradients: gradients are modified in place
     _ = nn.utils.clip_grad_norm_(encoder.parameters(), clip)
     _ = nn.utils.clip_grad_norm_(decoder.parameters(), clip)
 
-    # Adjust model weights
     encoder_optimizer.step()
     decoder_optimizer.step()
 
@@ -434,40 +410,32 @@ def train(input_variable, lengths, target_variable, mask, max_target_len, encode
 
 
 def trainIters(model_name, voc, pairs, encoder, decoder, encoder_optimizer, decoder_optimizer, embedding, encoder_n_layers, decoder_n_layers, save_dir, n_iteration, batch_size, print_every, save_every, clip, corpus_name, loadFilename):
-
-    # Load batches for each iteration
     training_batches = [batch_to_train_data(voc, [random.choice(pairs) for _ in range(batch_size)])
                       for _ in range(n_iteration)]
 
     if loadFilename:
         start_iteration = checkpoint['iteration'] + 1
 
-    # Initializations
     print('Initializing ...')
     start_iteration = 1
     print_loss = 0
     if loadFilename:
         start_iteration = checkpoint['iteration'] + 1
 
-    # Training loop
     print("Training...")
     for iteration in range(start_iteration, n_iteration + 1):
         training_batch = training_batches[iteration - 1]
-        # Extract fields from batch
         input_variable, lengths, target_variable, mask, max_target_len = training_batch
 
-        # Run a training iteration with batch
         loss = train(input_variable, lengths, target_variable, mask, max_target_len, encoder,
                      decoder, embedding, encoder_optimizer, decoder_optimizer, batch_size, clip)
         print_loss += loss
 
-        # Print progress
         if iteration % print_every == 0:
             print_loss_avg = print_loss / print_every
             print("Iteration: {}; Percent complete: {:.1f}%; Average loss: {:.4f}".format(iteration, iteration / n_iteration * 100, print_loss_avg))
             print_loss = 0
 
-        # Save checkpoint
         if (iteration % save_every == 0):
             directory = os.path.join(save_dir, model_name, corpus_name, '{}-{}_{}'.format(encoder_n_layers, decoder_n_layers, hidden_size))
             if not os.path.exists(directory):
@@ -491,44 +459,42 @@ class GreedySearchDecoder(nn.Module):
         self.decoder = decoder
 
     def forward(self, input_seq, input_length, max_length):
-        # Forward input through encoder model
+
         encoder_outputs, encoder_hidden = self.encoder(input_seq, input_length)
-        # Prepare encoder's final hidden layer to be first hidden input to the decoder
+
         decoder_hidden = encoder_hidden[:decoder.n_layers]
-        # Initialize decoder input with SOS_token
+
         decoder_input = torch.ones(1, 1, device=device, dtype=torch.long) * SOS_token
-        # Initialize tensors to append decoded words to
+
         all_tokens = torch.zeros([0], device=device, dtype=torch.long)
         all_scores = torch.zeros([0], device=device)
-        # Iteratively decode one word token at a time
+
         for _ in range(max_length):
-            # Forward pass through decoder
+
             decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden, encoder_outputs)
-            # Obtain most likely word token and its softmax score
+
             decoder_scores, decoder_input = torch.max(decoder_output, dim=1)
-            # Record token and score
+
             all_tokens = torch.cat((all_tokens, decoder_input), dim=0)
             all_scores = torch.cat((all_scores, decoder_scores), dim=0)
-            # Prepare current token to be next decoder input (add a dimension)
+
             decoder_input = torch.unsqueeze(decoder_input, 0)
-        # Return collections of word tokens and scores
+
         return all_tokens, all_scores
 
 
 def evaluate(encoder, decoder, searcher, voc, sentence, max_length=MAX_LENGTH):
-    ### Format input sentence as a batch
-    # words -> indexes
+
     indexes_batch = [index_sentence(voc, sentence)]
-    # Create lengths tensor
     lengths = torch.tensor([len(indexes) for indexes in indexes_batch])
-    # Transpose dimensions of batch to match models' expectations
+
     input_batch = torch.LongTensor(indexes_batch).transpose(0, 1)
-    # Use appropriate device
+
     input_batch = input_batch.to(device)
     lengths = lengths.to(device)
-    # Decode sentence with searcher
+
     tokens, scores = searcher(input_batch, lengths, max_length)
-    # indexes -> words
+
     decoded_words = [voc.index2word[token.item()] for token in tokens]
     return decoded_words
 
@@ -588,18 +554,17 @@ if loadFilename:
     embedding_sd = checkpoint['embedding']
     voc.__dict__ = checkpoint['voc_dict']
 
-print('Building encoder and decoder ...')
-# Initialize word embeddings
+print('Building encoder and decoder')
 embedding = nn.Embedding(voc.num_words, hidden_size)
 if loadFilename:
     embedding.load_state_dict(embedding_sd)
-# Initialize encoder & decoder models
+
 encoder = EncoderRNN(hidden_size, embedding, encoder_n_layers, dropout)
 decoder = LuongAttnDecoderRNN(attn_model, embedding, hidden_size, voc.num_words, decoder_n_layers, dropout)
 if loadFilename:
     encoder.load_state_dict(encoder_sd)
     decoder.load_state_dict(decoder_sd)
-# Use appropriate device
+
 encoder = encoder.to(device)
 decoder = decoder.to(device)
 print('Models built and ready to go!')
@@ -612,36 +577,31 @@ n_iteration = 4000
 print_every = 1
 save_every = 500
 
-# Ensure dropout layers are in train mode
 encoder.train()
 decoder.train()
 
-# Initialize optimizers
 encoder.train()
 decoder.train()
 
-# Initialize optimizers
-print('Building optimizers ...')
+print('Building optimizers')
 encoder_optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
 decoder_optimizer = optim.Adam(decoder.parameters(), lr=learning_rate * decoder_learning_ratio)
 
-# Run training iterations
-print("Starting Training!")
+
+print("Start training")
 trainIters(model_name, voc, pairs, encoder, decoder, encoder_optimizer, decoder_optimizer,
            embedding, encoder_n_layers, decoder_n_layers, save_dir, n_iteration, batch_size,
            print_every, save_every, clip, corpus_name, loadFilename)
 
-# Set dropout layers to eval mode
+
 encoder.eval()
 decoder.eval()
 
-# Initialize search module
 searcher = GreedySearchDecoder(encoder, decoder)
 
-# Begin chatting (uncomment and run the following line to begin)
-evaluateInput(encoder, decoder, searcher, voc)
+# evaluateInput(encoder, decoder, searcher, voc)
 
-# Print some pairs to validate
+
 # print("\npairs:")
 # for pair in pairs[:10]:
 #     print(pair)
@@ -733,7 +693,21 @@ async def on_message(message):
     elif "!logout" in message.content.lower():
         await message.channel.send("Fine then...")
         await client.close()
+    elif "!msg" in message.content.lower():
+        try:
+            # Normalize sentence
+            input_sentence = normalize_string(message.content.replace("!msg", ""))
+            # Evaluate sentence
+            output_words = evaluate(encoder, decoder, searcher, voc, input_sentence)
+            # Format and print response sentence
+            output_words[:] = [x for x in output_words if not (x == 'EOS' or x == 'PAD')]
+            sentence = ''
+            for i in range(len(output_words)):
+                sentence += str(output_words[i]) + " "
+            await message.channel.send(sentence)
 
+        except KeyError:
+            print("Error: Encountered unknown word.")
 
 
 client.loop.create_task(user_metrics_background_task())
